25a2e24e6b55c4480dcb2781323f2f774d1b55de
Akash Kumar Prasad <akash22gupta33@gmail.com>
2021-12-24 21:16:12 +0530

    Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const changeCount = (value) => {
+    setCount(count + value)
+  }
+  const multi2 = () => {
+    setCount(count * 2)
+  }
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Counter:{count}</h2>
+      
+      <button onClick = {()=>changeCount(+1)}>Increment</button>
+      <button onClick = {()=>changeCount(-1)}>Decrement</button>
+      <button onClick = {multi2}>Double</button>
+      
     </div>
   );
 }
